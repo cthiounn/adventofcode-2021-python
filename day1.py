7e8052c8860ed5b7542deb43c79564d4e15e241a
cthiounn <thiounn.conrad@gmail.com>
2021-12-01 19:22:31 +0000

    init repo

---
+++ day1.py
@@ -0,0 +1,14 @@
+with open('data/my_input/1.in') as f:
+    lines = [  line.strip() for line in f]
+
+def part1(vlines):
+
+    return 0
+
+def part2(vlines):
+    
+
+    return 0
+
+print(part1(lines))
+print(part2(lines))
