beb0f5f5c9ed27af448f518fbb498e7a05486996
cthiounn <thiounn.conrad@gmail.com>
2021-12-01 19:54:37 +0000

    init other days

---
+++ day13.py
@@ -0,0 +1,11 @@
+with open('data/my_input/13.in') as f:
+    lines = [  line.strip() for line in f]
+
+def part1(vlines):
+    return 0
+
+def part2(vlines):
+    return 0
+
+print(part1(lines))
+print(part2(lines))
