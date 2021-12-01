ae8b39e27b6b4cf7760ce79c300a85aa5e5f9836
cthiounn <thiounn.conrad@gmail.com>
2021-12-01 19:41:08 +0000

    day1

--- day1.py
+++ day1.py
@@ -2,13 +2,22 @@ with open('data/my_input/1.in') as f:
     lines = [  line.strip() for line in f]
 
 def part1(vlines):
-
-    return 0
+    num_increased=0
+    old=int(vlines[0])
+    for _,j in enumerate(vlines[1:]):
+        if int(j)>old:
+            num_increased+=1
+        old=int(j)
+    return num_increased
 
 def part2(vlines):
-    
-
-    return 0
+    num_increased=0
+    for i in range(len(vlines)):
+        if i==0 or i >=len(vlines)-2:
+            continue
+        if sum(map(int,vlines[i-1:i+2])) <sum(map(int,vlines[i:i+3])):
+            num_increased+=1
+    return num_increased
 
 print(part1(lines))
 print(part2(lines))
