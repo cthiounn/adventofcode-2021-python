228dd3a730c2caa3edf4e6e0c5daa4b147e0a2d8
cthiounn <thiounn.conrad@gmail.com>
2021-12-07 05:21:30 +0000

    day7

--- day7.py
+++ day7.py
@@ -1,11 +1,28 @@
 with open('data/my_input/7.in') as f:
     lines = [  line.strip() for line in f]
 
-def part1(vlines):
-    return 0
+def part1(vl):
+    maxl=max([abs(int(i)) for i in  vl[0].split(',')])
+ 
+    
+    minnn=maxl*10000
+    for j in range(maxl):
+        mm=sum([abs(int(i)-j) for i in  vl[0].split(',')])
+        if mm<=minnn:
+            minnn=mm
+    return minnn
 
-def part2(vlines):
-    return 0
+def part2(vl):
+    maxl=max([abs(int(i)) for i in  vl[0].split(',')])
+ 
+    
+    minnn=maxl*100000
+    for j in range(maxl):
+        mm=sum([(abs(int(i)-j)+1)*(abs(int(i)-j))/2 for i in  vl[0].split(',')])
+        if mm<=minnn:
+            minnn=mm
+        
+    return int(minnn)
 
 print(part1(lines))
 print(part2(lines))
