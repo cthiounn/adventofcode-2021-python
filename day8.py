c7806c1dd68212ca7b8c174387160c52da23fdbe
cthiounn <thiounn.conrad@gmail.com>
2021-12-08 07:38:21 +0000

    day8 part2

--- day8.py
+++ day8.py
@@ -1,30 +1,4 @@
-from collections import Counter
-import re
-from collections import defaultdict
-
-m= lambda s: re.findall(r'\d+',s)
-
-LAMBDA={
-    "acedgfb": 8,
-"cdfbe": 5,
-"gcdfa": 2,
-"fbcad": 3,
-"dab": 7,
-"cefabd": 9,
-"cdfgeb": 6,
-"eafb": 4,
-"cagedb": 0,
-"ab": 1,
-"cdfeb": 5,
-"fcadb": 3,
-"cdfeb": 5,
-"cdbaf": 3
-}
-LAMDA2=dict()
-for i,j in enumerate(LAMBDA.items()):
-    LAMDA2["".join(sorted(j[0]))]=j[1]
-with open('data/test/8.test') as f:
-#with open('data/my_input/8.in') as f:
+with open('data/my_input/8.in') as f:
     lines = [  line.strip() for line in f]
 
 def part1(v):
@@ -58,23 +32,28 @@ def part2(v):
                     if 1 in invdic and len(set(jj)&set(invdic[1]))==1 :
                         mydict["".join(sorted(jj))]=6
                         invdic[6]=jj
-                    if 6 in invdic and len(set(jj)&set(invdic[6]))==4 :
+                    elif 3 in invdic and len(set(jj)&set(invdic[3]))==5 :
                         mydict["".join(sorted(jj))]=9
                         invdic[9]=jj
-                    if 6 in invdic and 9 in invdic and set(jj)!=set(invdic[6]):
+                    elif 6 in invdic and 9 in invdic:
                         mydict["".join(sorted(jj))]=0
                         invdic[0]=jj
                 elif len(jj)==5:
-                    if 6 in invdic and len(set(jj)&set(invdic[6]))==5:
-                        mydict["".join(sorted(jj))]=5
-                        invdic[5]=jj
-                    elif 6 in invdic and len(set(jj)&set(invdic[6]))==4:
-                        mydict["".join(sorted(jj))]=2
-                        invdic[2]=jj
-                    elif 1 in invdic and len(set(jj)&set(invdic[1]))==2:
+                    if 1 in invdic and len(set(jj)&set(invdic[1]))==2:
                         mydict["".join(sorted(jj))]=3
                         invdic[3]=jj
-                print(mydict)
+                    elif 6 in invdic and len(set(jj)&set(invdic[6]))==5:
+                        mydict["".join(sorted(jj))]=5
+                        invdic[5]=jj
+                    else:
+                    #elif 6 in invdic and len(set(jj)&set(invdic[6]))==4:
+                        mydict["".join(sorted(jj))]=2
+                        invdic[2]=jj
+        st=""
+        for _, jj in enumerate(j.split("|")[1].split()):
+            st+=str(mydict["".join(sorted(jj))])
+        
+        count+=int(st)
     return count
 
 print(part1(lines))
