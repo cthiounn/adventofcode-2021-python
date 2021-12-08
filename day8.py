3710133b5c81867fa39c20786bb792ae883a4fb6
cthiounn <thiounn.conrad@gmail.com>
2021-12-08 06:51:26 +0000

    WIP in commute

--- day8.py
+++ day8.py
@@ -39,16 +39,42 @@ def part2(v):
     count=0
     for i,j in enumerate(v):
         mydict=dict()
-        for _,jj in enumerate(j.replace('|','').split()):
-            if len(jj)==2:
-                mydict["".join(sorted(jj))]=1
-            elif len(jj)==3:
-                mydict["".join(sorted(jj))]=7
-            elif len(jj)==4:
-                mydict["".join(sorted(jj))]=4
-            elif len(jj)==7:
-                mydict["".join(sorted(jj))]=8
-            print(mydict)
+        invdic=dict()
+        while len(mydict)!=10:
+            for _,jj in enumerate(j.replace('|','').split()):
+                if len(jj)==2:
+                    mydict["".join(sorted(jj))]=1
+                    invdic[1]=jj
+                elif len(jj)==3:
+                    mydict["".join(sorted(jj))]=7
+                    invdic[7]=jj
+                elif len(jj)==4:
+                    mydict["".join(sorted(jj))]=4
+                    invdic[4]=jj
+                elif len(jj)==7:
+                    mydict["".join(sorted(jj))]=8
+                    invdic[8]=jj
+                elif len(jj)==6 :
+                    if 1 in invdic and len(set(jj)&set(invdic[1]))==1 :
+                        mydict["".join(sorted(jj))]=6
+                        invdic[6]=jj
+                    if 6 in invdic and len(set(jj)&set(invdic[6]))==4 :
+                        mydict["".join(sorted(jj))]=9
+                        invdic[9]=jj
+                    if 6 in invdic and 9 in invdic and set(jj)!=set(invdic[6]):
+                        mydict["".join(sorted(jj))]=0
+                        invdic[0]=jj
+                elif len(jj)==5:
+                    if 6 in invdic and len(set(jj)&set(invdic[6]))==5:
+                        mydict["".join(sorted(jj))]=5
+                        invdic[5]=jj
+                    elif 6 in invdic and len(set(jj)&set(invdic[6]))==4:
+                        mydict["".join(sorted(jj))]=2
+                        invdic[2]=jj
+                    elif 1 in invdic and len(set(jj)&set(invdic[1]))==2:
+                        mydict["".join(sorted(jj))]=3
+                        invdic[3]=jj
+                print(mydict)
     return count
 
 print(part1(lines))
