f69faee5e17bd66e58d83e91cc2fbf96b333aae6
cthiounn <thiounn.conrad@gmail.com>
2021-12-16 12:30:11 +0000

    d16 WIP

--- day16.py
+++ day16.py
@@ -1,9 +1,3 @@
-from collections import deque
-from collections import defaultdict
-import re
-
-
-m= lambda s: re.findall(r'\d+',s)
 with open('data/test/16.test') as f:
     testlines = [  line.strip() for line in f]
 with open('data/my_input/16.in') as f:
@@ -11,7 +5,6 @@ with open('data/my_input/16.in') as f:
 
 
 CODE={
-
 "0" : "0000",
 "1" : "0001",
 "2" : "0010",
@@ -28,56 +21,98 @@ CODE={
 "D" : "1101",
 "E" : "1110",
 "F" : "1111",
-    
 }
 
+# def readsimple4(st):
+#     paquet_version=int(st[:3],base=2)
+#     type_id=int(st[3:6],base=2)
+#     rest=st[6:]
+#     st2=""
+#     for i in range(int(len(rest)/5)):
+#         st2+=rest[0+i*5:5+i*5][1:]
+#     return (paquet_version,type_id,int(st2,base=2))
+
 def readsimple4(st):
-    paquet_version=int(st[:3],base=2)
-    type_id=int(st[3:6],base=2)
-    rest=st[6:]
-    st2=""
-    for i in range(int(len(rest)/5)):
-        st2+=rest[0+i*5:5+i*5][1:]
-    return (paquet_version,type_id,int(st2,base=2))
+    li=list()
+    while st:
+        paquet_version,st=st[:3],st[3:]
+        type_id,st=st[:3],st[3:]
+        paquet_version=int(paquet_version,base=2)
+        type_id=int(type_id,base=2)
+        c=True
+        finalst=""
+        while c:
+            f,four,st=st[0],st[1:5],st[5:]
+            if f=="0":
+                c=False
+            finalst+=four
+        li.append((paquet_version,type_id,int(finalst,base=2)))
+    return li
+
+def readsimple44(st,num):
+    li=list()
+    i=0
+    while i<num:
+        i+=1
+        paquet_version,st=st[:3],st[3:]
+        type_id,st=st[:3],st[3:]
+        paquet_version=int(paquet_version,base=2)
+        type_id=int(type_id,base=2)
+        c=True
+        finalst=""
+        while c:
+            f,four,st=st[0],st[1:5],st[5:]
+            if f=="0":
+                c=False
+            finalst+=four
+        li.append((paquet_version,type_id,int(finalst,base=2)))
+    return li
+
 def part1(v):
     st=""
     count=0
-    d=dict()
-    dd=defaultdict(int)
-    li=[]
     for i,j in enumerate(v[0]):
         st+=(CODE[j])
-    paquet_version=int(st[:3],base=2)
-    type_id=int(st[3:6],base=2)
-    print(paquet_version,type_id)
+    
+    paquet_version,st=st[:3],st[3:]
+    type_id,st=st[:3],st[3:]
+    paquet_version=int(paquet_version,base=2)
+    type_id=int(type_id,base=2)
+    count+=paquet_version
     if type_id!=4:
-        length_type_id=st[6]
-        if length_type_id=="0":
-            howmuchreadbyte=int(st[7:7+15],base=2)
-            rest=st[7+15:7+15+howmuchreadbyte]
-            print(rest)
-        elif length_type_id=="1":
+        f,st=st[0],st[1:]
+        if f=="0":
+            l,st=st[:15],st[15:]
             
-            howmuchsubpaquet=int(st[7:7+11],base=2)
-            print(howmuchsubpaquet)
-            rest=(st[7+11:])
-            for i in  range(howmuchsubpaquet):
-                nextbyte=rest[i*11:11+i*11]
-                print(readsimple4(nextbyte))
+            lsize=(int(l,base=2))
+            st=st[:lsize]
+            li=readsimple4(st)
+            count+=sum([k[0] for v,k in enumerate(li)])
+            print(li)
+        elif f=="1":
+            l,st=st[:11],st[11:]
+            lsize=(int(l,base=2))
+            li=readsimple44(st,lsize)
+            count+=sum([k[0] for v,k in enumerate(li)])
+            print(li)
 
     else:
-        rest=st[6:]
-        st2=""
-        for i in range(int(len(rest)/5)):
-            st2+=rest[0+i*5:5+i*5][1:]
-        print(int(st2,base=2))
-        print(rest,len(rest))
+        c=True
+        finalst=""
+        while c:
+            f,four,st=st[0],st[1:5],st[5:]
+            if f=="0":
+                c=False
+            finalst+=four
+        print(int(finalst,base=2))
+    
+    
     return count
 
 def part2(v):
     return 0
 
 print("part1 test output",part1(testlines))
-#print("part1 my output",part1(lines))
+print("part1 my output",part1(lines))
 print("part2 test output",part2(testlines))
 print("part2 my output",part2(lines))
