ff953f331c5d44d66387015b2237408a5703a8df
cthiounn <thiounn.conrad@gmail.com>
2021-12-22 07:00:01 +0000

    d22 p2

--- day22.py
+++ day22.py
@@ -27,8 +27,52 @@ def part1(v):
                             del d[(k,l,m)]
     return sum(d.values())
 
-def part2(vlines):
-    return 0
+
+def operation(l,cube,add):
+    (xmin,xmax,ymin,ymax,zmin,zmax)=cube
+    li=[]
+    for i,j in enumerate(l):
+        (xmin1,xmax1,ymin1,ymax1,zmin1,zmax1)=j
+        notintersect= xmax<xmin1 or xmax1 <xmin or  ymax<ymin1 or ymax1 <ymin or zmax<zmin1 or zmax1 <zmin 
+        if notintersect :
+            li.append(j)
+        else:
+            if xmin > xmin1:
+                li.append((xmin1, xmin-1, ymin1, ymax1, zmin1, zmax1))
+            if xmax < xmax1:
+                li.append((xmax+1, xmax1, ymin1, ymax1, zmin1, zmax1))
+
+            if ymin > ymin1:
+                li.append((max(xmin,xmin1),min(xmax,xmax1),ymin1, ymin-1, zmin1, zmax1))
+            if ymax < ymax1:
+                li.append((max(xmin,xmin1),min(xmax,xmax1),ymax+1, ymax1, zmin1, zmax1))
+
+            if zmin > zmin1:
+                li.append((max(xmin,xmin1),min(xmax,xmax1),max(ymin,ymin1),min(ymax,ymax1), zmin1, zmin -1 ))
+            if zmax < zmax1:
+                li.append((max(xmin,xmin1),min(xmax,xmax1),max(ymin,ymin1),min(ymax,ymax1), zmax+1, zmax1 ))
+    if add==1:
+        li.append(cube)
+    return li
+
+def part2(v):
+    list_of_cubes=[]
+    for i,j in enumerate(v):
+        d=defaultdict(int)
+        turn=0
+        if "on" in j :
+            turn=1
+        m= lambda s: re.findall(r'-?\d+',s)
+        xmin,xmax,ymin,ymax,zmin,zmax=list(map(int,m(j)))
+        list_of_cubes=operation(list_of_cubes,(xmin,xmax,ymin,ymax,zmin,zmax),turn)
+
+    count=0
+    for _,cube in enumerate(list_of_cubes):
+        a,b,c,d,e,f=cube
+        count+= (b-a+1)*(d-c+1)*(f-e+1)
+
+    return count
+        
 
 print(part1(lines))
 print(part2(lines))
