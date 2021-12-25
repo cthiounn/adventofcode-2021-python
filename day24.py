d0c575b77c5d9c598c6001914f901638bb91099c
cthiounn <thiounn.conrad@gmail.com>
2021-12-25 10:28:14 +0000

    d25

--- day24.py
+++ day24.py
@@ -1,8 +1,9 @@
+import functools
 with open('data/my_input/24.in') as f:
     lines = [  line.strip() for line in f]
 
 
-
+@functools.lru_cache(maxsize=0)
 def runprog(d,v,st):
     for i,j in enumerate(v):
         op, v1,*v2 = j.split()
@@ -12,8 +13,6 @@ def runprog(d,v,st):
                 d[v1]=j
                 b, st2 =runprog(d.copy(),v[i+1:],st+str(j))
                 if b:
-                    
-                    print(d,st)
                     return True,st2
         elif op=="mul":
             v2=v2[0]
@@ -53,8 +52,7 @@ def runprog(d,v,st):
     if d["z"]==1:
         return True,st
     return False,st
-def part1(v):
-    
+def part1(v):    
     d={
         'x':0,
         'y':0,
@@ -62,51 +60,6 @@ def part1(v):
         'w':0,
     }
     return runprog(d,v,"")
-    # for i,j in enumerate(v):
-    #     op, v1,*v2 = j.split()
-
-    #     if op=="inp":
-    #         d[v1]=1
-    #         continue
-    #     elif op=="mul":
-    #         v2=v2[0]
-    #         if v2.lstrip('-').isnumeric():
-    #             d[v1]*=int(v2)
-    #         else:
-    #             d[v1]*=d[v2]
-    #     elif op=="add":
-    #         v2=v2[0]
-
-    #         if v2.lstrip('-').isnumeric():
-    #             d[v1]+=int(v2)
-    #         else:
-    #             d[v1]+=d[v2]
-    #     elif op=="mod":
-    #         v2=v2[0]
-            
-    #         if v2.lstrip('-').isnumeric():
-    #             d[v1]%=int(v2)
-    #         else:
-    #             d[v1]%=d[v2]
-    #     elif op=="div":
-    #         v2=v2[0]
-
-    #         if v2.lstrip('-').isnumeric():
-    #             d[v1]//=int(v2)
-    #         else:
-    #             d[v1]//=d[v2]
-    #     elif op=="eql":
-    #         v2=v2[0]
-
-    #         if v2.lstrip('-').isnumeric():
-    #             d[v1]= 1 if d[v1]==int(v2) else 0
-    #         else:
-    #             d[v1]= 1 if d[v1]==d[v2] else 0 
-    
-    # if d["z"]==1:
-    #     print("good")
-
-    # return 0
 
 def part2(v):
     return 0
--- day25.py
+++ day25.py
@@ -1,11 +1,63 @@
+
+with open('data/test/25.test') as f:
+    testlines = [  line.strip() for line in f]
 with open('data/my_input/25.in') as f:
     lines = [  line.strip() for line in f]
 
-def part1(vlines):
-    return 0
 
-def part2(vlines):
-    return 0
+def move(d,east=1):
+    
+    maxx=max([k[0] for k in d.keys()])
+    minx=min([k[0] for k in d.keys()])
+    maxy=max([k[1] for k in d.keys()])
+    miny=min([k[1] for k in d.keys()])
 
+    newd=dict()
+    for j in range(miny,maxy+1):
+        jnext=j+1
+        jnext%=maxy+1
+        for i in range(minx,maxx+1):
+            inext=i+1
+            inext%=maxx+1
+
+            if d[(i,j)]==1 and east==1: 
+                if d[(i,jnext)]==0:
+                    newd[(i,j)]=0
+                    newd[(i,jnext)]=d[(i,j)]
+                else:
+                    newd[(i,j)]=d[(i,j)]
+            elif d[(i,j)]==2 and east==0:
+                if d[(inext,j)]==0:
+                    newd[(i,j)]=0
+                    newd[(inext,j)]=d[(i,j)]
+                else:
+                    newd[(i,j)]=d[(i,j)]
+            else:
+                if (i,j) not in newd:
+                    newd[(i,j)]=d[(i,j)]
+    return newd
+
+def part1(v):
+    d=dict()
+    for i,j in enumerate(v):
+        for ii, jj in enumerate(j):
+            if jj==">":
+                d[(i,ii)]=1
+            elif jj=="v":
+                d[(i,ii)]=2
+            else:
+                d[(i,ii)]=0
+    
+    l=d.copy()
+    i=0
+    while True:
+        old=l.copy()
+        l=move(l,1)
+        l=move(l,0)
+        i+=1
+        if old==l:
+            break
+    return i
+
+print(part1(testlines))
 print(part1(lines))
-print(part2(lines))
