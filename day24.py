7b532de7992a449c604a855f70693b239d29ea4e
cthiounn <thiounn.conrad@gmail.com>
2021-12-28 12:12:17 +0000

    d24 p2 slow algo

--- day24.py
+++ day24.py
@@ -1,3 +1,4 @@
+from collections import deque
 import functools
 with open('data/my_input/24.in') as f:
     lines = [  line.strip() for line in f]
@@ -51,12 +52,15 @@ def runprog2(mp,starti):
         d['y']=0
         d['z']=mp
         d['w']=jj
+        has_break=False
         for i,j in enumerate(lines[starti:]):
             op, v1,*v2 = j.split()
             if op=="inp":
-                b, st2 =runprog(d['z'],starti+i+1)
+                b, st2 =runprog2(d['z'],starti+i+1)
+                
                 if b:
                     return True,str(jj)+st2
+                has_break=True
                 break
             else:
                 v2=v2[0]
@@ -76,11 +80,48 @@ def runprog2(mp,starti):
                 elif op=="eql":
                     d[v1]= 1 if d[v1]==value else 0
         
-        if d["z"]==0:
+        if d["z"]==0 and not has_break:
             return True,str(jj)
     return False,None
 def part2(v):
     return runprog2(0,1)
 
+
+def test(v,num):
+    queue=deque()
+    for i,j in enumerate(num):
+        queue.append(j)
+    d=dict()    
+    d['x']=0
+    d['y']=0
+    d['z']=0
+    d['w']=0
+    for i,j in enumerate(v):
+        print(d)
+        op, v1,*v2 = j.split()
+        if op=="inp":
+            d['w']=int(queue.popleft())
+        else:
+            v2=v2[0]
+            if v2.lstrip('-').isnumeric():
+                value=int(v2)
+            else:
+                value=d[v2]
+        
+            if op=="mul":
+                d[v1]*=value
+            elif op=="add":
+                d[v1]+=value
+            elif op=="mod":        
+                d[v1]%=value
+            elif op=="div":
+                d[v1]//=value
+            elif op=="eql":
+                d[v1]= 1 if d[v1]==value else 0
+    print(d['z'])
+
+# test(lines,"51131616112781")
+
+# test(lines,"52793949489991")
 # print(part1(lines))
 print(part2(lines))
